754a8a822ce25d1eb4fe8e476eaa0c127f0e0fdc
dam <dam@203P10.mshome.net>
2025-03-20 14:04:28 +0100

    COMMIT

---
+++ src/refactorizar/Clase1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package refactorizar;
+
+/**
+ *
+ * @author dam
+ */
+public class Clase1 {
+    
+    public void hola(){
+        System.out.println("BUENASSS");
+    }
+    public void manu(){
+        System.out.println("manuel anton isar");
+    }
+}
